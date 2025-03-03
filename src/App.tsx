58d739f873707b3fd70287a54455f8779b2e5b26
KaugaKauga <zacharias.holm@gmail.com>
2025-03-03 11:00:42 +0100

    dynamic colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Graph } from "./components/Graph";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen space-y-4 w-screen bg-violet-200">
+    <div className="flex flex-col items-center justify-center h-screen space-y-4 w-screen bg-violet-100">
       <div>
-        <h1 className="text-4xl font-bold text-violet-400">HabitGraph</h1>
+        <h1 className="text-4xl font-bold text-violet-300">HabitGraph</h1>
       </div>
       <Graph />
       <Graph />
--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -9,15 +9,31 @@ const generateDummyData = () => {
   return data;
 };
 
+const getColorClass = (count: number): string => {
+  const colorMap: { [key: number]: string } = {
+    0: "bg-purple-100",
+    1: "bg-purple-200",
+    2: "bg-purple-300",
+    3: "bg-purple-400",
+    4: "bg-purple-500",
+    5: "bg-purple-600",
+    6: "bg-purple-700",
+    7: "bg-purple-800",
+    8: "bg-purple-900",
+  };
+  return colorMap[count] || "bg-purple-100"; // Default to lightest if count is invalid
+};
+
 const Graph = () => {
   const data = generateDummyData();
-
   return (
     <div className="grid grid-cols-20 grid-rows-5 gap-1">
       {data.map((day, index) => (
         <div
           key={index}
-          className="w-4 h-4 bg-emerald-400 hover:shadow-md hover:shadow-yellow-400"
+          className={`w-4 h-4 ${getColorClass(
+            day.count
+          )} hover:shadow-md hover:shadow-yellow-400`}
           title={`${day.date.toDateString()}: ${day.count} contributions`}
         />
       ))}
