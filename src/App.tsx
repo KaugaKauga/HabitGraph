e86d22054cdcd2f79c5e25d9e7b3f82d4b6a48a6
KaugaKauga <zacharias.holm@gmail.com>
2025-04-15 20:18:00 +0200

    daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { useNavigate } from "@tanstack/react-router";
 import { GraphSection } from "./components/GraphSection";
 import { useGraphStore } from "./store";
 
 function App() {
   const { graphs } = useGraphStore();
-  console.log({ graphs });
+  const navigate = useNavigate();
+
   return (
     <>
       <div>
         <h1 className="text-4xl font-bold text-stone-300">HabitGraph</h1>
       </div>
+      <h2
+        className="text-2xl font-bold text-stone-300"
+        onClick={() => navigate({ to: "/create-category" })}
+      >
+        Create category
+      </h2>
       {Object.entries(graphs).map(([key, graph]) => (
         <GraphSection key={key} graph={graph} />
       ))}
