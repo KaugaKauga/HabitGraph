5b4b79f15296c82a70d5da18c928b4ceb178b354
KaugaKauga <zacharias.holm@gmail.com>
2025-04-29 20:33:15 +0200

    back from add habit

--- src/features/create-category/CreateHabit.tsx
+++ src/features/create-category/CreateHabit.tsx
@@ -1,7 +1,8 @@
 import { useRef, useState } from "react";
 import { useGraphStore } from "../../store";
-import { useNavigate } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { ColorType, colorTypes, getColorClass } from "../../utils/colorUtils";
+import arrowLeft from "../../assets/arrow-left.svg";
 
 const CreateHabit = () => {
   const { addGraph } = useGraphStore();
@@ -27,53 +28,60 @@ const CreateHabit = () => {
   };
 
   return (
-    <div className="space-y-2 h-full flex flex-col items-center justify-center">
-      <fieldset className="fieldset w-full">
-        <legend className="fieldset-legend">Habit</legend>
-        <input
-          ref={nameRef}
-          type="text"
-          className="input w-full"
-          placeholder="Name of the habit I want to track"
-          minLength={2}
-          maxLength={20}
-        />
-        <p className="fieldset-label">
-          Something like: Workout, Eat Kiwi, Veg meals
-        </p>
-      </fieldset>
-      <div className="flex justify-between w-full">
-        <fieldset className="fieldset">
-          <label className="fieldset-label">
-            <input ref={trueFalseRef} type="checkbox" className="checkbox" />
-            True / False
-          </label>
-        </fieldset>
-
-        <fieldset className="fieldset">
-          <label className="fieldset-label">
-            <input
-              ref={higherIsBetterRef}
-              type="checkbox"
-              defaultChecked
-              className="checkbox"
-            />
-            Higher is better
-          </label>
-        </fieldset>
+    <div className="flex flex-col h-full justify-center">
+      <div className="flex justify-between mb-8">
+        <Link className="link" to="/">
+          <img src={arrowLeft} alt="Home" className="size-[1.2em]" />
+        </Link>
       </div>
-      <div className="flex gap-4 flex-wrap">
-        {colorTypes.map((color) => (
-          <div
-            className={`${getColorClass(3, color)} ${color === selectedColor ? "w-6 h-6" : "w-4 h-4"}`}
-            key={color}
-            onClick={() => setColor(color)}
+      <div className="flex-1 overflow-y-auto space-y-4 justify-center flex flex-col items-center p-2">
+        <fieldset className="fieldset w-full">
+          <legend className="fieldset-legend">Habit</legend>
+          <input
+            ref={nameRef}
+            type="text"
+            className="input w-full"
+            placeholder="Name of the habit I want to track"
+            minLength={2}
+            maxLength={20}
           />
-        ))}
+          <p className="fieldset-label">
+            Something like: Workout, Eat Kiwi, Veg meals
+          </p>
+        </fieldset>
+        <div className="flex justify-between w-full">
+          <fieldset className="fieldset">
+            <label className="fieldset-label">
+              <input ref={trueFalseRef} type="checkbox" className="checkbox" />
+              True / False
+            </label>
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <label className="fieldset-label">
+              <input
+                ref={higherIsBetterRef}
+                type="checkbox"
+                defaultChecked
+                className="checkbox"
+              />
+              Higher is better
+            </label>
+          </fieldset>
+        </div>
+        <div className="flex gap-4 flex-wrap">
+          {colorTypes.map((color) => (
+            <div
+              className={`${getColorClass(3, color)} ${color === selectedColor ? "w-6 h-6" : "w-4 h-4"}`}
+              key={color}
+              onClick={() => setColor(color)}
+            />
+          ))}
+        </div>
+        <button className="btn btn-wide" onClick={handleCreate}>
+          Create
+        </button>
       </div>
-      <button className="btn btn-wide" onClick={handleCreate}>
-        Create
-      </button>
     </div>
   );
 };
